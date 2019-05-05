b32e7c2f0be951d29fe02f4105d3f0dbf54ad917
Alexy Long <alexy.long@orange.fr>
2019-05-05 16:50:49 +0200

    Put EditBoneConstraintProperty in its own file

--- editboneconstraint/properties/__init__.py
+++ editboneconstraint/properties/__init__.py
@@ -1,13 +1,6 @@
-import bpy
+from .edit_bone_constraint import EditBoneConstraintProperty
 
 
-class EditBoneConstraintProperty(bpy.types.PropertyGroup):
-    type: bpy.props.StringProperty(name="Type")
-    target: bpy.props.StringProperty(name="Target")
-    bone: bpy.props.StringProperty(name="Bone")
-    influence: bpy.props.FloatProperty(name="Influence", default=1.0, min=0.0, max=1.0)
-    head_tail: bpy.props.FloatProperty(name="Head/Tail", default=0.0, min=0.0, max=1.0)
-
-    offset: bpy.props.FloatVectorProperty(name="Offset", size=16, subtype="MATRIX")
-    initial_matrix: bpy.props.FloatVectorProperty(name="Original Matrix", size=16, subtype="MATRIX")
-    initial_length: bpy.props.FloatProperty(name="Initial Length")
+__all__ = [
+    'EditBoneConstraintProperty',
+]
