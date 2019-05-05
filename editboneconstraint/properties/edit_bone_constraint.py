18b5c88aec1afbf22b9b691ea7cba1efb07bdfc6
Loïc Pinsard <muream@gmail.com>
2019-05-06 01:23:18 +0200

    Implement the constraint gui completely

--- editboneconstraint/properties/edit_bone_constraint.py
+++ editboneconstraint/properties/edit_bone_constraint.py
@@ -6,6 +6,7 @@ class EditBoneConstraintProperty(bpy.types.PropertyGroup):
     target: bpy.props.StringProperty(name="Target")
     bone: bpy.props.StringProperty(name="Bone")
     influence: bpy.props.FloatProperty(name="Influence", default=1.0, min=0.0, max=1.0)
+    mute: bpy.props.BoolProperty(name="Mute", default=False)
+    show_expanded: bpy.props.BoolProperty(name="Show Expanded", default=True)
     head_tail: bpy.props.FloatProperty(name="Head/Tail", default=0.0, min=0.0, max=1.0)
-
     offset: bpy.props.FloatVectorProperty(name="Offset", size=16, subtype="MATRIX")
--- editboneconstraint/ui/constraint_stack.py
+++ editboneconstraint/ui/constraint_stack.py
@@ -18,21 +18,71 @@ class ConstraintStackPanel(bpy.types.Panel):
             "editbone.constraint_add", "type", text="Add Edit Bone Constraint"
         )
 
-        for constraint in context.active_bone.constraints:
-            self.draw_constraint(context, constraint)
+        for i, constraint in enumerate(context.active_bone.constraints):
+            self.draw_constraint(context, constraint, i)
 
-    def draw_constraint(self, context, constraint):
+    def draw_constraint(self, context, constraint, index):
         layout = self.layout
         box = layout.box()
 
+        # HEADER
         row = box.row()
-        row.prop(constraint, "name")
 
-        row = box.row()
-        row.prop_search(constraint, "target", context.object.data, "edit_bones")
+        col = row.column()
+        if constraint.show_expanded:
+            col.prop(
+                constraint,
+                "show_expanded",
+                icon="TRIA_DOWN",
+                icon_only=True,
+                emboss=False,
+            )
+        else:
+            col.prop(
+                constraint,
+                "show_expanded",
+                icon="TRIA_RIGHT",
+                icon_only=True,
+                emboss=False,
+            )
 
-        row = box.row()
-        row.prop(constraint, "head_tail")
+        col = row.column()
+        col.label(text=constraint.type)
 
-        row = box.row()
-        row.prop(constraint, "influence")
+        col = row.column()
+        if not constraint.target:
+            col.alert = True
+        col.prop(constraint, "name", text="")
+
+        col = row.column()
+        if constraint.mute:
+            col.prop(constraint, "mute", icon="HIDE_ON", icon_only=True, emboss=False)
+        else:
+            col.prop(constraint, "mute", icon="HIDE_OFF", icon_only=True, emboss=False)
+
+        if len(context.active_bone.constraints) > 1:
+            col = row.column()
+            reorder_row = col.row(align=True)
+            if index == 0:
+                reorder_row.operator("editbone.move_constraint_down", text="", icon="TRIA_DOWN")
+            elif index == len(context.active_bone.constraints) - 1:
+                reorder_row.operator("editbone.move_constraint_up", text="", icon="TRIA_UP")
+            else:
+                reorder_row.operator("editbone.move_constraint_down", text="", icon="TRIA_DOWN")
+                reorder_row.operator("editbone.move_constraint_up", text="", icon="TRIA_UP")
+
+
+        col = row.column()
+        col.operator("editbone.delete_constraint", text="", icon="X", emboss=False)
+
+        if constraint.show_expanded:
+            # BODY
+            row = box.row()
+            row.prop_search(constraint, "target", context.object.data, "edit_bones")
+
+            row = box.row()
+            row.label(text="Head/Tail:")
+            row.prop(constraint, "head_tail", text="", slider=True)
+
+            row = box.row()
+            row.prop(constraint, "influence", slider=True)
