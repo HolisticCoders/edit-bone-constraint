bcbff19daa5575c5c30ed8f35ac2d570673a6c4e
Alexy Long <alexy.long@orange.fr>
2019-05-05 18:19:42 +0200

    Removed unused EditBoneConstraintProperty initial_matrix and initial_length

--- editboneconstraint/properties/edit_bone_constraint.py
+++ editboneconstraint/properties/edit_bone_constraint.py
@@ -9,5 +9,3 @@ class EditBoneConstraintProperty(bpy.types.PropertyGroup):
     head_tail: bpy.props.FloatProperty(name="Head/Tail", default=0.0, min=0.0, max=1.0)
 
     offset: bpy.props.FloatVectorProperty(name="Offset", size=16, subtype="MATRIX")
-    initial_matrix: bpy.props.FloatVectorProperty(name="Original Matrix", size=16, subtype="MATRIX")
-    initial_length: bpy.props.FloatProperty(name="Initial Length")
