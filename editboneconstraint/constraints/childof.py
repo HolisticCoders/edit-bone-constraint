01893e918718701590c2797f4ac94ed0be83f058
Loïc Pinsard <muream@gmail.com>
2019-05-05 16:10:19 +0200

    Use the target matrix and length instead of individual attributes

--- editboneconstraint/constraints/childof.py
+++ editboneconstraint/constraints/childof.py
@@ -7,10 +7,7 @@ class ChildOf(AbstractConstraint):
     def evaluate(self):
         if not self.target:
             return
-
-        # FIXME: For some reason Y and Z locations are swapped, X is Fine
         new_mat = self.target.matrix @ self.offset
-        # new_mat = self.offset @ self.target.matrix
         self.bone.matrix = new_mat
 
 
--- editboneconstraint/constraints/copytransform.py
+++ editboneconstraint/constraints/copytransform.py
@@ -7,14 +7,8 @@ class CopyTransform(AbstractConstraint):
     def evaluate(self):
         if not self.target:
             return
-        self.bone.tail[0] = self.target.tail[0]
-        self.bone.tail[1] = self.target.tail[1]
-        self.bone.tail[2] = self.target.tail[2]
-        self.bone.head[0] = self.target.head[0]
-        self.bone.head[1] = self.target.head[1]
-        self.bone.head[2] = self.target.head[2]
-        self.bone.roll = self.target.roll
-
+        self.bone.matrix = self.target.matrix.copy()
+        self.bone.length = self.target.length
 
 
 exported_constraints = [CopyTransform]
