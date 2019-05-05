ee0f6cea8aebb07be6dae9417960aa67eacd3ead
Alexy Long <alexy.long@orange.fr>
2019-05-05 18:20:16 +0200

    Removed leftover all_constraints list

--- editboneconstraint/constraints/__init__.py
+++ editboneconstraint/constraints/__init__.py
@@ -5,5 +5,3 @@ from .constraint_from_property import instanciate_constraint_from_property
 __all__ = [
     'instanciate_constraint_from_property',
 ]
-
-all_constraints = _get_all_constraints_classes()
