4515702bc47c6d2183192fd80047af6b4c814bcf
Loïc Pinsard <muream@gmail.com>
2019-05-05 16:34:53 +0200

    add lerp function to utils

--- editboneconstraint/utils.py
+++ editboneconstraint/utils.py
@@ -1,4 +1,7 @@
 def flatten_matrix(matrix):
     dim = len(matrix)
-    return [matrix[j][i] for i in range(dim) 
-                      for j in range(dim)]
+    return [matrix[j][i] for i in range(dim) for j in range(dim)]
+
+
+def lerp(a, b, factor):
+    return a + factor * (b - a)
