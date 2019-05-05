9f1303a4ef9efd438f8b0802b5f54f614fc45159
Alexy Long <alexy.long@orange.fr>
2019-05-05 18:22:16 +0200

    Added ConstraintManager class
    It is in charge of blending constraints together to set
    edit bones new matrices and lengths.

--- editboneconstraint/constraints/__init__.py
+++ editboneconstraint/constraints/__init__.py
@@ -1,7 +1,9 @@
 from .constraint_from_property import instanciate_constraint_from_property
+from .constraint_manager import ConstraintManager
 
 
 
 __all__ = [
     'instanciate_constraint_from_property',
+    'ConstraintManager',
 ]
---
+++ editboneconstraint/constraints/constraint_manager.py
@@ -0,0 +1,30 @@
+from editboneconstraint.utils import lerp
+from .constraint_from_property import instanciate_constraint_from_property
+
+
+class ConstraintManager:
+
+    def __init__(self, bone):
+        self._bone = bone
+
+    def evaluate(self):
+        constraints = self._instantiate_constraints()
+        if not constraints:
+            return
+
+        new_matrix = self._bone.initial_matrix
+        new_length = self._bone.initial_length
+        for constraint in constraints:
+            constraint_matrix, constraint_length = constraint.evaluate()
+            new_matrix = new_matrix.lerp(constraint_matrix, constraint.influence)
+            new_length = lerp(new_length, constraint_length, constraint.influence)
+
+        self._bone.matrix = new_matrix
+        self._bone.length = new_length
+
+    def _instantiate_constraints(self):
+        constraints = []
+        for constraint_prop in self._bone.constraints:
+            constraint = instanciate_constraint_from_property(constraint_prop)
+            constraints.append(constraint)
+        return constraints
